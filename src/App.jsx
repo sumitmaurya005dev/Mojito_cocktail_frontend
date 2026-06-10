ff2c3c433fa13680fb05fcc505a95719e3686783
sumitmaurya005.dev <sumitmaurya005.dev@gmail.com>
2026-06-10 20:12:46 +0530

    implement about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from 'gsap'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
        <Navbar/>
        <Hero/>
        <Cocktails/>
+       <About/>
     </main>
   )
 }
